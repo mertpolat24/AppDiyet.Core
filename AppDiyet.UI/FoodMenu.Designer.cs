3f3e11d8ba91b34ffe09daa00e730a8e83031d10
Yusuf Keskin <yusuf.keskin@bilgeadamboost.com>
2024-12-11 01:25:44 +0300

    LabelC5BABA

---
+++ AppDiyet.UI/FoodMenu.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace AppDiyet.UI
+{
+    partial class FoodMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox1 = new PictureBox();
+            listBox1 = new ListBox();
+            label7 = new Label();
+            label8 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(187, 152);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(205, 27);
+            textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(187, 189);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(205, 27);
+            textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(187, 235);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(205, 27);
+            textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(187, 284);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(205, 27);
+            textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(187, 335);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(205, 27);
+            textBox6.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(187, 108);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(205, 28);
+            comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic);
+            label1.Location = new Point(53, 152);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Yemek Adı";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic);
+            label2.Location = new Point(69, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 23);
+            label2.TabIndex = 2;
+            label2.Text = "Kategori";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic);
+            label3.Location = new Point(77, 235);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Protein";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic);
+            label4.Location = new Point(88, 189);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 23);
+            label4.TabIndex = 2;
+            label4.Text = "Kalori";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic);
+            label5.Location = new Point(63, 335);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 23);
+            label5.TabIndex = 2;
+            label5.Text = "Açıklama";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Italic);
+            label6.Location = new Point(83, 284);
+            label6.Name = "label6";
+            label6.Size = new Size(57, 23);
+            label6.TabIndex = 2;
+            label6.Text = "Miktar";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(615, 456);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Ekle";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(742, 456);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "Sil";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(863, 456);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 3;
+            button3.Text = "Güncelle";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(730, 98);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(227, 260);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(453, 98);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(239, 264);
+            listBox1.TabIndex = 5;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(453, 63);
+            label7.Name = "label7";
+            label7.Size = new Size(96, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Yemek Listesi";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(730, 63);
+            label8.Name = "label8";
+            label8.Size = new Size(117, 20);
+            label8.TabIndex = 6;
+            label8.Text = "Yemek Fotoğrafı";
+            // 
+            // FoodMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 653);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(listBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox6);
+            Controls.Add(textBox3);
+            Controls.Add(textBox5);
+            Controls.Add(textBox2);
+            Name = "FoodMenu";
+            Text = "FoodMenu";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private ComboBox comboBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private ListBox listBox1;
+        private Label label7;
+        private Label label8;
+    }
+}
---
+++ AppDiyet.UI/OgunCrud.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace AppDiyet.UI
+{
+    partial class OgunCrud
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listBox1 = new ListBox();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            pictureBox1 = new PictureBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(35, 86);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(255, 484);
+            listBox1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(781, 155);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 2;
+            button5.Text = "Hesapla";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(658, 585);
+            button6.Name = "button6";
+            button6.Size = new Size(110, 29);
+            button6.TabIndex = 2;
+            button6.Text = "Öğün Kaydet";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(791, 585);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 2;
+            button7.Text = "Yeni Yemek";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(713, 270);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(209, 187);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(477, 86);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 4;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(724, 86);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(151, 28);
+            comboBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(477, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 20);
+            label1.TabIndex = 6;
+            label1.Text = "Yemekler";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(724, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Porsiyon";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(534, 585);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Öğün Ekle";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(534, 377);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(534, 430);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(534, 270);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(534, 323);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(378, 277);
+            label3.Name = "label3";
+            label3.Size = new Size(102, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Toplam Kalori";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(378, 330);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Toplam Protein";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(378, 384);
+            label5.Name = "label5";
+            label5.Size = new Size(76, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Kategorisi";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(378, 437);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Açıklama";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(35, 44);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 9;
+            // 
+            // OgunCrud
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 653);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button1);
+            Controls.Add(button5);
+            Controls.Add(listBox1);
+            Name = "OgunCrud";
+            Text = "OgunCrud";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox listBox1;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private PictureBox pictureBox1;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+    }
+}
