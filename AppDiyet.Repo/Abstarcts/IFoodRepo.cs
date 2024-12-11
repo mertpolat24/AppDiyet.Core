3f39dac34e4981a0e09314adc5754ff2e8e46042
Mert POLAT <24mertpolat@gmail.com>
2024-12-12 01:32:02 +0300

    LastUpdate/1.1

--- AppDiyet.Repo/Abstarcts/IFoodRepo.cs
+++ AppDiyet.Repo/Abstarcts/IFoodRepo.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppDiyet.Core.Concretes;
+using AppDiyet.Core.Enums;
 
 namespace AppDiyet.Repo.Abstarcts
 {
     public interface IFoodRepo : IBaseRepo<Food>
     {
-        double CalculatePortionsCalories(int id, Food amount, Food portionType); 
-        double CalculatePortionsProteins(int id, Food amount, Food portionType); 
+        double CalculatePortionsCalories(int id, Food amount, PortionType portionType); 
+        double CalculatePortionsProteins(int id, Food amount, PortionType portionType); 
     }
 }
--- AppDiyet.UI/OgunCrud.cs
+++ AppDiyet.UI/OgunCrud.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using AppDiyet.Core.Concretes;
+using AppDiyet.Core.Enums;
+using AppDiyet.Service.Abstracts;
+using AppDiyet.Service.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +17,37 @@ namespace AppDiyet.UI
 {
     public partial class OgunCrud : Form
     {
-        public OgunCrud()
+        IMealsService mealsService = new MealsService();
+        IFoodService foodService = new FoodService();
+        int userId = 0;
+        public OgunCrud(int id)
         {
             InitializeComponent();
+            userId = id;
+            GetAllFoods();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void GetAllFoods()
+        {
+            var foodsList = Enum.GetValues(typeof(PortionType));
+            comboBox1.DataSource = foodsList;
+            
+            
+            var portions = foodService.CalculatePortionsProteins(userId,porsiyonNumericUpDown.Value,foodsList.ToString());
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void porsiyonNumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
